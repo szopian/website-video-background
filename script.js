e04102f3b3ef0e91a2375c6c8550a0b9d466dd5e
Adri Ana <szopiangreg@gmail.com>
2021-03-12 22:42:42 +0000

    add close menu function

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const menuToggle = document.querySelector(".toggle");
 const showcase = document.querySelector(".showcase");
+const closeMenu = document.querySelector(".close-menu");
 
 menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
+  showcase.classList.toggle("active");
+});
+
+closeMenu.addEventListener("click", () => {
   menuToggle.classList.toggle("active");
   showcase.classList.toggle("active");
 });
